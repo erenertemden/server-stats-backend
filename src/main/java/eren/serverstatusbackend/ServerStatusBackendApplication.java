42fe88fcb586a23209b9b0854eb97f3a88227d32
Eren Ertem <mail.erenertem@gmail.com>
2025-02-13 11:05:48 +0300

    initialize spring boot

---
+++ src/main/java/eren/serverstatusbackend/ServerStatusBackendApplication.java
@@ -0,0 +1,13 @@
+package eren.serverstatusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStatusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStatusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eren/serverstatusbackend/ServerStatusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eren.serverstatusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStatusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
